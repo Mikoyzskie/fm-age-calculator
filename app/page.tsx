98e56d03b16681d7f04f5f9856546c8d4549e55a
Mikoy <rapidstrike13@gmail.com>
2023-07-29 22:00:24 +0800

    design fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,27 @@ export default function Home() {
       <form action="" className={styles.form}>
         <div className={styles.input_container}>
           <div>
-            <label htmlFor="day">DAY</label>
-            <input type="text" id="day" placeholder='DD'/>
+            <label htmlFor="day" className={styles.labels}>DAY</label>
+            <input type="text" id="day" placeholder='DD' className={styles.inputs}/>
           </div>
           <div>
-            <label htmlFor="month">MONTH</label>
-            <input type="text" id="month" placeholder='MM'/>
+            <label htmlFor="month" className={styles.labels}>MONTH</label>
+            <input type="text" id="month" placeholder='MM' className={styles.inputs}/>
           </div>
           <div>
-            <label htmlFor="year">YEAR</label>
-            <input type="text" id="year" placeholder='YYYY'/>
+            <label htmlFor="year" className={styles.labels}>YEAR</label>
+            <input type="text" id="year" placeholder='YYYY' className={styles.inputs}/>
           </div>
         </div>
         <div className={styles.submit}>
           <hr />
-          <button type="submit"><Image src={'./icon-arrow.svg'} alt='icon arrow' width={100} height={100}/></button>
+          <button type="submit" className={styles.arrow}><Image src={'./icon-arrow.svg'} alt='icon arrow' width={50} height={50}/></button>
         </div>
       </form>
-      <section>
-        <h1><span>--</span> years</h1>
-        <h1><span>--</span> months</h1>
-        <h1><span>--</span> days</h1>
+      <section className={styles.results}>
+        <h1><span className={styles.date}>--</span> years</h1>
+        <h1><span className={styles.date}>--</span> months</h1>
+        <h1><span className={styles.date}>--</span> days</h1>
       </section>
     </main>
   )
