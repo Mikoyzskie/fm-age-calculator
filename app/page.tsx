b0b137d563b8c023114f56113161e69cd439bd48
Mikoy <rapidstrike13@gmail.com>
2023-07-31 15:06:00 +0800

    minor update

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,52 @@
 "use client"
 import Image from 'next/image'
 import styles from './page.module.scss'
-import { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
+
+interface FormData {
+  day: number;
+  month: number;
+  year: number;
+}
 
 export default function Home() {
-
-  const [day, setDay] = useState('');
 
   const currentDate = new Date();
 
   const currentDay = currentDate.getDate();
-  const currentMonth = currentDate.getMonth() + 1; // Adding 1 because months are zero-indexed
+  const currentMonth = currentDate.getMonth() + 1;
   const currentYear = currentDate.getFullYear();
 
-  function handleSubmit(event: any){
-    event.preventDefault();
-    console.log('Current Day:', currentDay);
-    console.log('Current Month:', currentMonth);
-    console.log('Current Year:', currentYear);
-  }
+  const [formData, setFormData] = useState<FormData>({
+    day: 0,
+    month: 0,
+    year: 0,
+  });
+
+  const [required, setRequired] = useState('');
+  
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(formData.day === 0 || !formData.day){
+      console.log(formData.day);
+      setRequired("required"); 
+    }
+    else if(formData.month === 0 || !formData.month){
+      console.log(formData.month);
+      setRequired("required"); 
+    }
+    console.log(formData);
+  };
+
 
   return (
     <main className={styles.main}>
@@ -27,15 +54,18 @@ export default function Home() {
         <div className={styles.input_container}>
           <div>
             <label htmlFor="day" className={styles.labels}>DAY</label>
-            <input type="number" id="day" name='day' placeholder='DD' className={styles.inputs} autoComplete='off'/>
+            <input type="number" id="day" name='day' placeholder='DD' className={`${styles.inputs} ${required && styles.required}`} autoComplete='off' onChange={handleChange} />
+            <span className={styles.warning}>Must be a valid day</span>
           </div>
           <div>
             <label htmlFor="month" className={styles.labels}>MONTH</label>
-            <input type="number" id="month" name='month' placeholder='MM' className={styles.inputs} autoComplete='off'/>
+            <input type="number" id="month" name='month' placeholder='MM' className={styles.inputs} autoComplete='off' onChange={handleChange} />
+            <span className={styles.warning}>Must be a valid day</span>
           </div>
           <div>
             <label htmlFor="year" className={styles.labels}>YEAR</label>
-            <input type="number" id="year" name='year' placeholder='YYYY' className={styles.inputs} autoComplete='off'/>
+            <input type="number" id="year" name='year' placeholder='YYYY' className={styles.inputs} autoComplete='off' onChange={handleChange} />
+            <span className={styles.warning}>Must be a valid day</span>
           </div>
         </div>
         <div className={styles.submit}>
@@ -48,6 +78,7 @@ export default function Home() {
         <h1><span className={styles.date}>--</span> months</h1>
         <h1><span className={styles.date}>--</span> days</h1>
       </section>
+     
     </main>
   )
 }
