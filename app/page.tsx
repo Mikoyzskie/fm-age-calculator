e54f8805b9a1d13308d6f7d28491c423af0e7092
Mikoyzskie <rapidstrike13@gmail.com>
2024-01-05 12:50:44 +0800

    working calculator and warning

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,23 @@
 import Image from 'next/image'
 import './page.scss'
 import React, { useState, ChangeEvent, FormEvent } from 'react';
-
+import { clsx } from 'clsx';
 
 interface FormData {
-  day: number;
-  month: number;
-  year: number;
+  day: any;
+  month: any;
+  year: any;
 }
 
 export default function Home() {
 
-  const currentDate = new Date();
-
-  const currentDay = currentDate.getDate();
-  const currentMonth = currentDate.getMonth() + 1;
-  const currentYear = currentDate.getFullYear();
-
   const [formData, setFormData] = useState<FormData>({
-    day: 0,
-    month: 0,
-    year: 0,
+    day: '',
+    month: '',
+    year: ''
   });
 
-  const [required, setRequired] = useState('');
-  const [daywarn, setDaywarn] = useState('');
-  const [dayerror, setDayerror] = useState('hidden');
-  
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({
@@ -36,55 +27,154 @@ export default function Home() {
     });
   };
 
+  function checkDays(year: any, month: any) {
+    return new Date(year, month, 0).getDate();
+  }
+
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  const [errorMessageYear, setErrorMessageYear] = useState("")
+  const [errorMessageMonth, setErrorMessageMonth] = useState("")
+  const [errorMessageDay, setErrorMessageDay] = useState("")
+
+  const [day, setDay] = useState("--")
+  const [month, setMonth] = useState("--")
+  const [year, setYear] = useState("--")
+
+  const [dayError, setDayError] = useState(false)
+  const [monthError, setMonthError] = useState(false)
+  const [yearError, setYearError] = useState(false)
+
+  function calculateAge(year: any, month: any, day: any) {
+    let years = currentDate.getFullYear() - year;
+    let months = (currentDate.getMonth() + 1) - month;
+    let days = currentDate.getDate() - day;
+
+    if (months < 0 || (months === 0 && currentDate.getDate() < day)) {
+      years--;
+      months = (12 + currentDate.getMonth() + 1 - month) % 12;
+    }
+
+    if (days < 0) {
+      const prevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 0);
+      days = prevMonth.getDate() - day + currentDate.getDate();
+      months--;
+    }
+
+    setDay(days.toString())
+    setMonth(months.toString())
+    setYear(years.toString())
+  }
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(!formData.day || formData.day == 0){
-      console.log(formData.day);
-      setDayerror("");
-      setDaywarn("This field is required");
-      setRequired("required"); 
+
+    setDayError(false)
+    setMonthError(false)
+    setYearError(false)
+    setErrorMessageDay("")
+    setErrorMessageMonth("")
+    setErrorMessageYear("")
+
+    const validDays = checkDays(formData.year, formData.month);
+
+    if (!formData.day) {
+      setDayError(true)
+      setErrorMessageDay("This field is required")
+    } else if (!formData.month) {
+      setMonthError(true)
+      setErrorMessageMonth("This field is required")
+    } else if (!formData.year) {
+      setYearError(true)
+      setErrorMessageYear("This field is required")
+    } else if (formData.year > currentYear) {
+      setErrorMessageYear("Must be in the past")
+      setYearError(true)
+    } else if (formData.month > 12) {
+      setMonthError(true)
+      setErrorMessageMonth("Must be a valid month")
+    } else if (formData.day > 31) {
+      setDayError(true)
+      setErrorMessageDay("Must be a valid day")
+    } else if (formData.day > validDays) {
+      setDayError(true)
+      setMonthError(true)
+      setYearError(true)
+      setErrorMessageDay("Must be a valid date")
+    } else {
+      calculateAge(formData.year, formData.month, formData.day)
     }
-    else{
-      console.log(formData.day);
-      setRequired("");
-      setDayerror("hidden");
-      setDaywarn("");
-    }
-   
+
+
   };
 
 
   return (
     <main className="main">
-      <form action="" className="form" onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <div className="input_container">
           <div>
-            <label htmlFor="day" className="labels">DAY</label>
-            <input type="number" id="day" name='day' placeholder='DD' className={`inputs ${required}`} autoComplete='off' onChange={handleChange} />
-            <span className={`warning ${dayerror}`}>{daywarn}</span>
+            <label htmlFor="day" className={clsx("labels", dayError ? "required" : "")}>DAY</label>
+            <input
+              type="number"
+              id="day"
+              name='day'
+              placeholder='DD'
+              className={clsx(`inputs`, dayError ? "required" : "")}
+              autoComplete='off'
+              value={formData.day}
+              onChange={handleChange} />
+            <span className="warning">
+              {errorMessageDay}
+
+            </span>
           </div>
           <div>
-            <label htmlFor="month" className="labels">MONTH</label>
-            <input type="number" id="month" name='month' placeholder='MM' className={`inputs`} autoComplete='off' onChange={handleChange} />
-            <span className="warning">Must be a valid day</span>
+            <label htmlFor="month" className={clsx("labels", monthError ? "required" : "")}>MONTH</label>
+            <input
+              type="number"
+              id="month"
+              name='month'
+              placeholder='MM'
+              className={clsx(`inputs`, monthError ? "required" : "")}
+              autoComplete='off'
+              value={formData.month}
+              onChange={handleChange} />
+            <span className="warning">
+              {errorMessageMonth}
+            </span>
           </div>
           <div>
-            <label htmlFor="year" className="labels">YEAR</label>
-            <input type="number" id="year" name='year' placeholder='YYYY' className={`inputs`} autoComplete='off' onChange={handleChange} />
-            <span className="warning">Must be a valid day</span>
+            <label htmlFor="year" className={clsx("labels", yearError ? "required" : "")}>YEAR</label>
+            <input
+              type="number"
+              id="year"
+              name='year'
+              placeholder='YYYY'
+              className={clsx(`inputs`, yearError ? "required" : "")}
+              autoComplete='off'
+              value={formData.year}
+              onChange={handleChange} />
+            <span className="warning">
+              {/* {
+                yearError && "This field is required"
+              } */}
+              {errorMessageYear}
+            </span>
           </div>
         </div>
         <div className="submit">
           <hr />
-          <button type="submit" className="arrow"><Image className="icon_arrow" src={'./icon-arrow.svg'} alt='icon arrow' width={50} height={50}/></button>
+          <button type="submit" className="arrow"><Image className="icon_arrow" src={'./icon-arrow.svg'} alt='icon arrow' width={50} height={50} /></button>
         </div>
       </form>
       <section className="results">
-        <h1><span className="date">--</span> years</h1>
-        <h1><span className="date">--</span> months</h1>
-        <h1><span className="date">--</span> days</h1>
+        <h1><span className="date">{year}</span> years</h1>
+        <h1><span className="date">{month}</span> months</h1>
+        <h1><span className="date">{day}</span> days</h1>
       </section>
-     
+
     </main>
   )
 }
